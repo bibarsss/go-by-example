76342a4a9d59441099bdd385410fab3c7924370e
bibarsss <bibaryskoutepov@mail.ru>
2024-09-27 00:33:36 +0500

    Added 1-hello-world

---
+++ 1-hello-world/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	//fmt.Print("Enter number a: ")
+	//fmt.Scan(&a)
+
+	fmt.Print("Enter a: ")
+	aInput, err := fmt.Scanln(&a)
+	if err != nil {
+		return
+	}
+	_ = aInput
+
+	fmt.Print("Enter b: ")
+	bInput, err := fmt.Scanln(&b)
+	if err != nil {
+		return
+	}
+	_ = bInput
+
+	fmt.Println("Sum is:", a+b)
+}
